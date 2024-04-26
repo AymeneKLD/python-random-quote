b4347d1187fbcf485bbad9dea2b3c633cda374ba
Aymene Khaled <marcomayestro@gmail.com>
2024-04-26 10:56:54 +0000

    Update query `My first shared query`

--- 2444290-My first shared query.sql
+++ 2444290-My first shared query.sql
@@ -1,12 +1,12 @@
 create DATABASE spec;
-create table members 
-    ( 
-        id primary;
-        names;
-        phone number;
-        spec_num foreign;
-    )
-create table spec 
+#create table members 
+ #   ( 
+  #      id primary;
+   #     names;
+    #    phone number;
+     #   spec_num foreign;
+    #)
+create< table spec 
     {
         spec_num primary;
         num_of_students;
