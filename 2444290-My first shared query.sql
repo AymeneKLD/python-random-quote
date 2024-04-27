43d0ab1afc81ffdc2d7a09b85313e210c1673467
Aymene Khaled <marcomayestro@gmail.com>
2024-04-27 10:21:48 +0000

    Update query `My first shared query`

--- 2444290-My first shared query.sql
+++ 2444290-My first shared query.sql
@@ -1,15 +1,1 @@
-CREATE TABLE Students
-(
-    st_id INT PRIMARY KEY,
-    Name CHAR(20),
-    mark DECIMAL(4,2)
-);
-INSERT INTO Students VALUES(1, 'Aymene', 19.5);
-DESCRIBE Students;
-SELECT * FROM Students;
-DROP TABLE members;
-SHOW DATABASES;
-DROP DATABASE TST;
-USE sys;
-CREATE DATABASE TST;
-RENAME DATABASE tst TO UNVER;
+1
